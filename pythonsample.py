a3c70ea7ede8df244dff307e1c9f6e7e1daaf0ae
AktarChoudhury <159473899+AktarChoudhury@users.noreply.github.com>
2024-02-15 20:54:02 +0000

    Add files via upload

---
+++ pythonsample.py
@@ -0,0 +1,14 @@
+## this is a comment 
+pip install termcolor
+from termcolor install colored
+# thid 
+adj=input("adjective: ")
+verb1 = input("verb1: ")
+verb2= input("verb2:")
+famous_person=input("famous_persom:")
+##
+madlib=f"Computer programming is so {adj}, it makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}."
+print (madlib)
+# SECOND MADLIB
+madlib="computer programming is " + adj
+print(colored (madlib), color="red")
